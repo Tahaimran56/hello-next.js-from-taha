0cbf09ae33c3fe9993421000eac025a1415d4c30
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2024-10-19 17:06:53 +0500

    Add files via upload
    hello everyoone!
    i done my project hello next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello next.js from (taha qureshi)</h1>
+      <br />
+      <p>Next.js is a popular open-source React framework!
+         developed by Vercel that enables developers to build server-rendered React
+          applications.
+        </p>
+    </div>
+    );
+}
